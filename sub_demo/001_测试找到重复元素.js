2b9fc34417a034dccad4c94a076b206e9f60a8fa
jiaopi404 <1240659804@qq.com>
2021-02-02 12:21:13 +0800

    update daily push

---
+++ sub_demo/001_测试找到重复元素.js
@@ -0,0 +1,11 @@
+const arr = [1,2,3,4,1,2,2,2];
+const result = arr.reduce((prev, item) => {
+  if (!prev[item]) {
+    prev[item] = 1;
+  } else {
+    prev[item] === 1 && prev.result.push(item);
+    prev[item]++;
+  }
+  return prev;
+}, { result: [] }).result
+console.log(result);
