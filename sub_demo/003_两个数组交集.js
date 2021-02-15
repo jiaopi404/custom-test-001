4262f4aa00ff9219a667ebad638b86d7de3b5e9d
jiaopi404 <1240659804@qq.com>
2021-02-15 10:31:28 +0800

    update demo 两个数组的交集

---
+++ sub_demo/003_两个数组交集.js
@@ -0,0 +1,23 @@
+
+
+// Problem: 求两个数组的交集
+const firstArray = [2, 2, 4, 1];
+const secondArray = [1, 2, 0, 2];
+// 实现 intersection 函数
+// @interview start
+
+function intersection (arr1, arr2)  {
+  return arr1.filter(item => arr2.indexOf(item) !== -1).reduce((prev, item) => {
+    if (prev.indexOf(item) === -1) {
+      prev.push(item);
+    }
+    return prev;
+  }, []);
+}
+
+function intersection2 (arr1, arr2) {
+  return [...new Set(arr1.filter(item => arr2.indexOf(item) !== -1))];
+}
+
+console.log(intersection(firstArray, secondArray));
+console.log(intersection2(firstArray, secondArray));
