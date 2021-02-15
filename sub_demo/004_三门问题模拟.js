a8d69132ea38deef88ab6e84cb7fc689b5ac7e7b
jiaopi404 <1240659804@qq.com>
2021-02-15 17:28:24 +0800

    update 三门问题试验, 但是结果过于明朗

---
+++ sub_demo/004_三门问题模拟.js
@@ -0,0 +1,15 @@
+function tripleDoors () { // calculate the result after change the door you choose.
+    const doors = [0, 1, 2]; // doors, 0 is car, 1 & 2 is goat.
+    const pick = Math.floor(Math.random() * 3); // the number of door that I pick
+    return pick !== 0;
+}
+
+let count = 0;
+const total = 10000
+for (let i = 0; i < total; i++) {
+    if (tripleDoors()) {
+        count++;
+    }
+}
+
+console.log(count / total);
