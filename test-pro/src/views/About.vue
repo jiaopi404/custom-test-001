586ed822ed87984bbcfa0c5120d77f54f859c83b
jiaopi404 <1240659804@qq.com>
2021-09-06 11:02:00 +0800

    update daily push

--- test-pro/src/views/About.vue
+++ test-pro/src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <canvas ref="qrcodeCanvas" id="qrcodeCanvas" style="height: 200px; width: 200px;"></canvas>
+    <button @click="generateQRCode">生成二维码</button>
   </div>
 </template>
+
+<script>
+import QRCode from 'qrcode'
+const About = {
+  name: 'About',
+  methods: {
+    generateQRCode () {
+      console.log('生成 qr code', QRCode)
+      const data = {
+        page: 'pagesSub/rmb/add-rmb/index',
+        query: {
+          id: 123
+        }
+      }
+      console.log(`http://budget.joriving.com/mini_app/rmb?data=${JSON.stringify(data)}`)
+      QRCode.toCanvas(this.$refs.qrcodeCanvas, `http://budget.joriving.com/mini_app/rmb?data=${JSON.stringify(data)}`, function (error) {
+        if (error) console.error(error)
+        console.log('success!')
+      })
+    }
+  }
+}
+export default About
+</script>
+
+<style lang="css">
+.about {}
+</style>
