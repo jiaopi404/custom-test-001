e63183f61fdf9df9a162fc66b6be75a6583e0875
jiaopi404 <1240659804>
2021-09-09 01:16:19 +0800

    update add sb_demo

---
+++ sb_demo/src/main/java/com/jiaopi404/sb_demo_001/SbDemo001Application.java
@@ -0,0 +1,13 @@
+package com.jiaopi404.sb_demo_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemo001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDemo001Application.class, args);
+    }
+
+}
---
+++ sb_demo/src/test/java/com/jiaopi404/sb_demo_001/SbDemo001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiaopi404.sb_demo_001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDemo001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
