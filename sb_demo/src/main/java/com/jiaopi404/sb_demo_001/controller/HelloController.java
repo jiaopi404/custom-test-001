58c89fecd49fc28d0bef1ad4ce63b58a50808f33
jiaopi404 <1240659804@qq.com>
2021-09-13 20:22:35 +0800

    update 测试onlyoffice 太牛逼了

--- sb_demo/src/main/java/com/jiaopi404/sb_demo_001/controller/HelloController.java
+++ sb_demo/src/main/java/com/jiaopi404/sb_demo_001/controller/HelloController.java
@@ -1,2 +1,21 @@
-package com.jiaopi404.sb_demo_001.controller;public class HelloController {
+package com.jiaopi404.sb_demo_001.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @PostMapping(path = "/saveFile")
+    public String saveFile(HttpServletRequest httpServletRequest) {
+        try {
+            System.out.println("hello");
+            return "{'error':'no' }";
+        } catch (Exception e) {
+            return "error";
+        }
+    }
 }
