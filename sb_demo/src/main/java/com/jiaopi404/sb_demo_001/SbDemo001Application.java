ae5f528c04f8177af6dc3c27c7a7508f321be31c
jiaopi404 <1240659804@qq.com>
2021-09-30 18:15:09 +0800

    update 添加 MapperScan 注解，扫描 mapper 类

--- sb_demo/src/main/java/com/jiaopi404/sb_demo_001/SbDemo001Application.java
+++ sb_demo/src/main/java/com/jiaopi404/sb_demo_001/SbDemo001Application.java
@@ -2,8 +2,10 @@ package com.jiaopi404.sb_demo_001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.jiaopi404.sb_demo_001.mapper")
 public class SbDemo001Application {
 
     public static void main(String[] args) {
