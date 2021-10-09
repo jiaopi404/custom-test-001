36d324a5494fc583d5d16a79a994ff5859191cc1
jiaopi404 <1240659804>
2021-10-10 02:27:27 +0800

    update 还原测试文件

--- sb_demo/src/test/java/com/jiaopi404/sb_demo_001/SbDemo001ApplicationTests.java
+++ sb_demo/src/test/java/com/jiaopi404/sb_demo_001/SbDemo001ApplicationTests.java
@@ -1,13 +1,30 @@
 package com.jiaopi404.sb_demo_001;
 
+import com.jiaopi404.sb_demo_001.pojo.TestTable;
+import com.jiaopi404.sb_demo_001.service.TestTableService;
+import com.jiaopi404.sb_demo_001.utils.UUIDGetter;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbDemo001ApplicationTests {
 
+    @Autowired
+    private TestTableService testTableService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testTableAdd () {
+        TestTable testTable = new TestTable();
+        testTable.setId(UUIDGetter.getAsString());
+        testTable.setName("自动测试 + " + System.currentTimeMillis());
+        testTable.setAge(11);
+        testTableService.add(testTable);
+        int a = 100 / 0;
     }
 
 }
