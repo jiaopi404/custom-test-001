540bc66500ff84a011581b11ce152c96ea42531e
jiaopi404 <1240659804>
2021-11-23 00:35:54 +0800

    update daily push

--- ts/ts_007_模块/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var point_1 = require("./point");
-var pointFoo = new point_1.Point(3, 4);
-var pointBar = new point_1.Point();
-// 使用时，使用类; 多态？？？
-console.log(point_1.Point.getDistance(pointFoo, pointBar));
-console.log(pointFoo.getDistance(pointBar));
