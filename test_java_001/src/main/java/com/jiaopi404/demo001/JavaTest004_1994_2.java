cdb1da6b116f1ce4127ec9bc84db7eebe1d57383
jiaopi404 <1240659804>
2022-02-22 20:20:50 +0800

    update leetcode 1994 2 test

---
+++ test_java_001/src/main/java/com/jiaopi404/demo001/JavaTest004_1994_2.java
@@ -0,0 +1,8 @@
+package com.jiaopi404.demo001;
+
+public class JavaTest004_1994_2 {
+
+    public static void main(String[] args) {
+        JavaTest004_1994_2 javaTest004_1994_2 = new JavaTest004_1994_2();
+    }
+}
