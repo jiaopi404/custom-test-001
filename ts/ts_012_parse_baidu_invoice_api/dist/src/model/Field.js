2d3ce8443a26567537c5aee1b62c75d9690f038f
jiaopi404 <1240659804>
2022-03-08 23:09:48 +0800

    update 搭建

---
+++ ts/ts_012_parse_baidu_invoice_api/dist/src/model/Field.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Field = void 0;
+var Field = /** @class */ (function () {
+    function Field(name) {
+        this.name = name;
+    }
+    return Field;
+}());
+exports.Field = Field;
+//# sourceMappingURL=Field.js.map
