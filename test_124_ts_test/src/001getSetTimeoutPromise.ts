7456ab913c43ae2dcab73931876dd33aa5114eff
jiaopi404 <1240659804>
2022-03-20 09:45:41 +0800

    update add a log

--- test_124_ts_test/src/001getSetTimeoutPromise.ts
+++ test_124_ts_test/src/001getSetTimeoutPromise.ts
@@ -2,6 +2,7 @@ export const getSetTimeoutPromise = (miliseconds: number) => {
   return new Promise<void>((resolve, reject) => {
     setTimeout(() => {
       console.log("timeout miliseconds: ", miliseconds);
+      console.log("timeout miliseconds: ", miliseconds); // add a log
       resolve();
     }, miliseconds);
   });
